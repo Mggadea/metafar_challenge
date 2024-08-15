181c4d0428e79bc40757578954d71322fdc9be52
mggadea <matiasgaraygadea@outlook.com>
2024-08-15 19:11:55 -0300

    add StockItem component

---
+++ components/StockItem.tsx
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useNavigation } from "expo-router";
+
+const StockItem = ({ symbol, name, type, currency }) => {
+
+  const navigation = useNavigation()
+
+  const handleNavigate = (symbol:string) => {
+    navigation.navigate('Details')
+  }
+
+  return (
+    <View style={styles.itemContainer}>
+      <Text
+      onPress={()=>handleNavigate()}
+      style={[styles.headerCell, styles.stockSymbol]}>{symbol}</Text>
+      <Text style={styles.headerCell}>{name.slice(0, 20)}</Text>
+      <Text style={styles.headerCell} >{currency}</Text>
+      <Text style={styles.headerCell}>{type}</Text>
+    </View>
+  );
+};
+
+export default StockItem;
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    width: "100%",
+
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    borderBottomColor: "#E5E5E5",
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+  },
+  stockSymbol: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  headerCell: {
+    flex: 1,
+    padding: 10,
+    textAlign: "center",
+  },
+});
