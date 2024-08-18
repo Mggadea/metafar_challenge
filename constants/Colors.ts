dfe794ec7eb4f45483c075bb3dc28224f506fcec
mggadea <matiasgaraygadea@outlook.com>
2024-08-18 01:10:46 -0300

    change on colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,17 +1,17 @@
-const tintColorLight = '#2f95dc';
+const tintColorLight = '#06f';
 const tintColorDark = '#fff';
 
 export default {
   light: {
-    text: '#000',
+    text: '#2b2b2b',
     background: '#fff',
     tint: tintColorLight,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: '#f4f4f4',
+    background: '#191919',
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
