878aeb35b265d76b8d15d8c0ed94ec921add93af
mggadea <matiasgaraygadea@outlook.com>
2024-08-19 16:31:04 -0300

    add cache

--- hooks/useStockChart.ts
+++ hooks/useStockChart.ts
@@ -2,23 +2,49 @@ import { useState, useEffect } from "react";
 import { getStockData } from "@/services/stockScreenServices";
 import { dateFormat } from "../helpers/dateFormat";
 
-const useStockChart = (symbol: string, interval: string, date:string) => {
-  const [data, setData] = useState([]);
+// Definimos una interfaz para la caché
+interface Cache {
+  [key: string]: {
+    data: any[];
+    timestamp: number;
+  };
+}
+
+const cache: Cache = {};
+
+const useStockChart = (symbol: string, interval: string, date: string) => {
+  const [data, setData] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
+  const cacheKey = `${symbol}-${interval}-${date}`;
+
   const loadData = async () => {
-    setLoading(true)
+    setLoading(true);
+    if (cache[cacheKey] && (Date.now() - cache[cacheKey].timestamp < 60000)) { // 1 minuto de caché
+      // Si los datos están en caché y no son demasiado antiguos, usamos la caché
+      setData(cache[cacheKey].data);
+      setLoading(false);
+      return;
+    }
+
     try {
       const fetchedData = await getStockData(symbol, interval, date);
       if (fetchedData.data.status === "error") {
         setError(fetchedData.data.message);
+        setData([]);
       } else {
-        setError('')
+        setError(null);
         setData(fetchedData.data.values);
+        // Actualizamos la caché con los nuevos datos
+        cache[cacheKey] = {
+          data: fetchedData.data.values,
+          timestamp: Date.now(),
+        };
       }
     } catch (err) {
       setError("Failed to fetch data");
+      setData([]);
     } finally {
       setLoading(false);
     }
@@ -26,7 +52,7 @@ const useStockChart = (symbol: string, interval: string, date:string) => {
 
   useEffect(() => {
     loadData();
-  }, [interval]);
+  }, [symbol, interval, date]);
 
   const chartData = {
     labels: (
