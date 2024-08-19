b808e36df7f3778f4e337ea4283e92f7cda1133c
mggadea <matiasgaraygadea@outlook.com>
2024-08-19 19:16:18 -0300

    comment

--- hooks/useStockChart.ts
+++ hooks/useStockChart.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { getStockData } from "@/services/stockScreenServices";
 import { dateFormat } from "../helpers/dateFormat";
 
-// Definimos una interfaz para la caché
+
 interface Cache {
   [key: string]: {
     data: any[];
@@ -21,8 +21,7 @@ const useStockChart = (symbol: string, interval: string, date: string) => {
 
   const loadData = async () => {
     setLoading(true);
-    if (cache[cacheKey] && (Date.now() - cache[cacheKey].timestamp < 60000)) { // 1 minuto de caché
-      // Si los datos están en caché y no son demasiado antiguos, usamos la caché
+    if (cache[cacheKey] && (Date.now() - cache[cacheKey].timestamp < 60000)) { 
       setData(cache[cacheKey].data);
       setLoading(false);
       return;
@@ -36,7 +35,6 @@ const useStockChart = (symbol: string, interval: string, date: string) => {
       } else {
         setError(null);
         setData(fetchedData.data.values);
-        // Actualizamos la caché con los nuevos datos
         cache[cacheKey] = {
           data: fetchedData.data.values,
           timestamp: Date.now(),
